b93d4eae53803b0f60e1f25e6786d3c116c0371b
masprog2022 <masprog2022@gmail.com>
2024-08-08 14:27:25 +0100

    initial project

---
+++ src/main/java/com/masprogtech/ShopTechApplication.java
@@ -0,0 +1,13 @@
+package com.masprogtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopTechApplication.class, args);
+	}
+
+}
