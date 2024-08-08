dab3fdb1379214eb5d493e7116a4d74a42056663
masprog2022 <masprog2022@gmail.com>
2024-08-08 20:16:28 +0100

    feat: add methods in ProductRepository

--- src/main/java/com/masprogtech/repositories/ProductRepository.java
+++ src/main/java/com/masprogtech/repositories/ProductRepository.java
@@ -3,5 +3,16 @@ package com.masprogtech.repositories;
 import com.masprogtech.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Long, Product> {
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByCategoryName(String category);
+
+    List<Product> findByBrand(String brand);
+
+    List<Product> findByCategoryNameAndBrand(String category, String brand);
+
+    List<Product> findByName(String name);
+
+    List<Product> findByBrandAndName(String brand, String name);
 }
