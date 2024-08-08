899281acff4ca3c287b80a5082c516aa0b91d500
masprog2022 <masprog2022@gmail.com>
2024-08-08 14:56:12 +0100

    feat: create entities, product, image and category

---
+++ src/main/java/com/masprogtech/entities/Category.java
@@ -0,0 +1,25 @@
+package com.masprogtech.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    List<Product> products;
+}
