658c19e566ec8b6a05ae5d2570cbfe7bebacf60f
masprog2022 <masprog2022@gmail.com>
2024-08-09 08:24:33 +0100

    feat: create AddProductRequest

--- src/main/java/com/masprogtech/request/AddProductRequest.java
+++ src/main/java/com/masprogtech/request/AddProductRequest.java
@@ -1,2 +1,17 @@
-package com.masprogtech.request;public class AddProductRequest {
+package com.masprogtech.request;
+
+import com.masprogtech.entities.Category;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class AddProductRequest {
+    private Long id;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private int inventory;
+    private String description;
+    private Category category;
 }
