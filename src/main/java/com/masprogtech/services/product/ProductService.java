4ddaca36c2ad2509cf19df38df81f3880021f13d
masprog2022 <masprog2022@gmail.com>
2024-08-09 08:25:28 +0100

    feat: addProduct

--- src/main/java/com/masprogtech/services/product/ProductService.java
+++ src/main/java/com/masprogtech/services/product/ProductService.java
@@ -1,24 +1,54 @@
 package com.masprogtech.services.product;
 
+import com.masprogtech.entities.Category;
 import com.masprogtech.entities.Product;
 import com.masprogtech.exception.ProductNotFoundException;
+import com.masprogtech.repositories.CategoryRepository;
 import com.masprogtech.repositories.ProductRepository;
+import com.masprogtech.request.AddProductRequest;
+import jdk.jfr.Frequency;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService implements IProductService {
 
     private final ProductRepository productRepository;
 
-    public ProductService(ProductRepository productRepository) {
+    private final CategoryRepository categoryRepository;
+
+    public ProductService(ProductRepository productRepository, CategoryRepository categoryRepository) {
         this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     @Override
-    public Product addProduct(Product product) {
-      return null;
+    public Product addProduct(AddProductRequest request) {
+      // check if the category is found in the DB
+        // If yes, set it as the new product category
+        // if No, the save is as new category
+        // the set as the new product category.
+
+        Category category = Optional.ofNullable(categoryRepository.findByName(request.getCategory().getName()))
+                .orElseGet(() -> {
+                    Category newCategory = new Category(request.getCategory().getName());
+                    return categoryRepository.save(newCategory);
+                });
+        request.setCategory(category);
+        return productRepository.save(createProduct(request, category));
+    }
+
+    private Product createProduct(AddProductRequest request, Category category){
+       return new Product(
+               request.getName(),
+               request.getBrand(),
+               request.getPrice(),
+               request.getInventory(),
+               request.getDescription(),
+               category
+       );
     }
 
     @Override
