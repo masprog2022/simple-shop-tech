ad42f9816f9f469499df09d7639e1d85673f5967
masprog2022 <masprog2022@gmail.com>
2024-08-09 12:29:47 +0100

    feat: create CategoryService

--- src/main/java/com/masprogtech/services/category/CategoryService.java
+++ src/main/java/com/masprogtech/services/category/CategoryService.java
@@ -1,2 +1,51 @@
-package com.masprogtech.services.category;public class CategoryService {
+package com.masprogtech.services.category;
+
+import com.masprogtech.entities.Category;
+import com.masprogtech.exception.ResourceNotFoundException;
+import com.masprogtech.repositories.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService implements ICategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public Category getCategoryById(Long id) {
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Category Not found!"));
+    }
+
+    @Override
+    public Category getCategoryByName(String name) {
+        return categoryRepository.findByName(name);
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public Category addCategory(Category category) {
+        return null;
+    }
+
+    @Override
+    public Category updateCategory(Category category, Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteCategoryById(Long id) {
+       categoryRepository.findById(id).ifPresentOrElse(categoryRepository::delete, ()->{
+           throw new ResourceNotFoundException("Category not found!");
+       });
+    }
 }
