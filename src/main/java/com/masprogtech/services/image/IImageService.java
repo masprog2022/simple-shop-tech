209c3d02b0ed3b56444ef392e88443fd8f835d30
masprog2022 <masprog2022@gmail.com>
2024-08-10 13:26:37 +0100

    feat: create IImageService

--- src/main/java/com/masprogtech/services/image/IImageService.java
+++ src/main/java/com/masprogtech/services/image/IImageService.java
@@ -1,14 +1,17 @@
-package com.masprogtech.services.product;
+package com.masprogtech.services.image;
 
+import com.masprogtech.dtos.ImageDto;
 import com.masprogtech.entities.Image;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface IImageService {
     Image getImageById(Long id);
 
     void deleteImageById(Long id);
 
-    Image saveImage(MultipartFile file, Long productId);
+    List<ImageDto> saveImages(List<MultipartFile> files, Long productId);
 
     void updateImage(MultipartFile file, Long imageId);
 }
