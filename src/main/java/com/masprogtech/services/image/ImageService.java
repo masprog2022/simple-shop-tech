11d28e92a1dec697617fed326c25c9fbd22c07f6
masprog2022 <masprog2022@gmail.com>
2024-08-10 13:26:57 +0100

    feat: create ImageService

--- src/main/java/com/masprogtech/services/image/ImageService.java
+++ src/main/java/com/masprogtech/services/image/ImageService.java
@@ -1,4 +1,95 @@
-package com.masprogtech.services;
+package com.masprogtech.services.image;
 
-public class ImageService {
+import com.masprogtech.dtos.ImageDto;
+import com.masprogtech.entities.Image;
+import com.masprogtech.entities.Product;
+import com.masprogtech.exception.ResourceNotFoundException;
+import com.masprogtech.repositories.ImageRepository;
+import com.masprogtech.services.product.IProductService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.sql.rowset.serial.SerialBlob;
+import java.io.IOException;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ImageService implements IImageService {
+
+    private final ImageRepository imageRepository;
+    private final IProductService productService;
+
+    public ImageService(ImageRepository imageRepository, IProductService productService) {
+        this.imageRepository = imageRepository;
+        this.productService = productService;
+    }
+
+    @Override
+    public Image getImageById(Long id) {
+        return imageRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No image found with id: " + id));
+    }
+
+    @Override
+    public void deleteImageById(Long id) {
+      imageRepository.findById(id).ifPresentOrElse(imageRepository::delete, () -> {
+          throw new ResourceNotFoundException("No image found with: " +id);
+      });
+    }
+
+    @Override
+    public List<ImageDto> saveImages(List<MultipartFile> files, Long productId) {
+        Product product = productService.getProductById(productId);
+
+        List<ImageDto> savedImageDto = new ArrayList<>();
+
+        for (MultipartFile file: files){
+            try {
+
+                Image image = new Image();
+                image.setFileName(file.getOriginalFilename());
+                image.setFileType(file.getContentType());
+                image.setImage(new SerialBlob(file.getBytes()));
+                image.setProduct(product);
+
+                String buildDownloadUrl = "/api/v1/images/image/download/";
+                String downloadUrl = buildDownloadUrl+image.getId();
+                image.setDownloadUrl(downloadUrl);
+
+                Image savedImage = imageRepository.save(image);
+
+                savedImage.setDownloadUrl(buildDownloadUrl+savedImage.getId());
+                imageRepository.save(savedImage);
+
+                ImageDto imageDto = new ImageDto();
+                imageDto.setId(savedImage.getId());
+                imageDto.setFileName(savedImage.getFileName());
+                imageDto.setDownloadUrl(savedImage.getDownloadUrl());
+                savedImageDto.add(imageDto);
+
+
+
+            }catch (IOException | SQLException e){
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+        return savedImageDto;
+    }
+
+    @Override
+    public void updateImage(MultipartFile file, Long imageId) {
+      Image image = getImageById(imageId);
+
+      try {
+        image.setFileName(file.getOriginalFilename());
+        image.setFileType(file.getContentType());
+        image.setImage(new SerialBlob(file.getBytes()));
+        imageRepository.save(image);
+      }catch (IOException | SQLException e){
+         throw new RuntimeException(e.getMessage());
+      }
+    }
 }
