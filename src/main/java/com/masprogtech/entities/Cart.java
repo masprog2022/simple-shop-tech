61d39342cdf8404d4243278b3248333203eb2af7
masprog2022 <masprog2022@gmail.com>
2024-08-13 17:21:48 +0100

    feat: create Cart and CartItem entities

---
+++ src/main/java/com/masprogtech/entities/Cart.java
@@ -0,0 +1,23 @@
+package com.masprogtech.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal totalAmount = BigDecimal.ZERO;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<CartItem> items = new HashSet<>();
+}
