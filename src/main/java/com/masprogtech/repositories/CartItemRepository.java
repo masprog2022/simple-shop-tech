dbeb681cfe080c90557402ff423748f4d91d1f22
masprog2022 <masprog2022@gmail.com>
2024-08-13 17:58:01 +0100

    feat: create CartItemRepository

--- src/main/java/com/masprogtech/repositories/CartItemRepository.java
+++ src/main/java/com/masprogtech/repositories/CartItemRepository.java
@@ -1,2 +1,8 @@
-package com.masprogtech.repositories;public interface CartItemRepository {
+package com.masprogtech.repositories;
+
+import com.masprogtech.entities.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    void deleteAllByCartId(Long id);
 }
