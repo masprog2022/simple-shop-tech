d61574f894a5b02c867cbc0474cae0adddbdf1fb
masprog2022 <masprog2022@gmail.com>
2024-08-14 17:05:13 +0100

    feat: remove method getCartItem

--- src/main/java/com/masprogtech/services/cart/ICartItemService.java
+++ src/main/java/com/masprogtech/services/cart/ICartItemService.java
@@ -7,5 +7,5 @@ public interface ICartItemService {
     void removeItemFromCart(Long cartId, Long productId);
     void updateItemQuantity(Long cartId, Long productId, int quantity);
 
-    CartItem getCartItem(Long cartId, Long productId);
+
 }
