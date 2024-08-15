15c0b3badd799a4fe49ddda09632d21d324a19be
masprog2022 <masprog2022@gmail.com>
2024-08-15 12:30:43 +0100

    feat: create OrderService

--- src/main/java/com/masprogtech/services/order/OrderService.java
+++ src/main/java/com/masprogtech/services/order/OrderService.java
@@ -1,2 +1,78 @@
-package com.masprogtech.services.order;public class OrderService {
+package com.masprogtech.services.order;
+
+import com.masprogtech.entities.Cart;
+import com.masprogtech.entities.Order;
+import com.masprogtech.entities.OrderItem;
+import com.masprogtech.entities.Product;
+import com.masprogtech.enums.OrderStatus;
+import com.masprogtech.exception.ResourceNotFoundException;
+import com.masprogtech.repositories.OrderRepository;
+import com.masprogtech.repositories.ProductRepository;
+import com.masprogtech.services.cart.CartService;
+import org.modelmapper.ModelMapper;
+
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class OrderService implements IOrderService {
+    private final OrderRepository orderRepository;
+    private final ProductRepository productRepository;
+    private final CartService cartService;
+
+    private final ModelMapper modelMapper;
+
+    public OrderService(OrderRepository orderRepository, ProductRepository productRepository, CartService cartService, ModelMapper modelMapper) {
+        this.orderRepository = orderRepository;
+        this.productRepository = productRepository;
+        this.cartService = cartService;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public Order placeOrder(Long userId) {
+        return null;
+    }
+
+
+    private Order createOrder(Cart cart){
+        Order order = new Order();
+        // set the user ...
+        order.setOrderStatus(OrderStatus.PENDING);
+        order.setOrderDate(LocalDate.now());
+        return order;
+
+    }
+
+    private List<OrderItem> createOrderItems(Order order, Cart cart) {
+        return cart.getItems().stream()
+                .map(cartItem -> {
+                    Product product = cartItem.getProduct();
+                    product.setInventory(product.getInventory() - cartItem.getQuantity());
+                    productRepository.save(product);
+                    return new OrderItem(
+                            order,
+                            product,
+                            cartItem.getQuantity(),
+                            cartItem.getUnitPrice());
+                }).toList();
+    }
+
+    private BigDecimal calculatetotalAmount(List<OrderItem> orderItemList) {
+        return orderItemList.stream()
+                .map(item -> item.getPrice().multiply(new BigDecimal(item.getQuantity())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+
+    @Override
+    public Order getOrder(Long orderId) {
+        return orderRepository.findById(orderId).orElseThrow(
+                () -> new ResourceNotFoundException("Order not found!"));
+    }
+
+
 }
