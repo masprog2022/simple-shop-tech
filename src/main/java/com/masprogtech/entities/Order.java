eb79484c238de2e418224b7a0ff499cef33fddfa
masprog2022 <masprog2022@gmail.com>
2024-08-15 11:45:07 +0100

    feat: create Order entity

--- src/main/java/com/masprogtech/entities/Order.java
+++ src/main/java/com/masprogtech/entities/Order.java
@@ -1,2 +1,30 @@
-package com.masprogtech.entities;public class Order {
+package com.masprogtech.entities;
+
+import com.masprogtech.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+    private LocalDate orderDate;
+    private BigDecimal totalAmount;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<OrderItem> orderItems = new HashSet<>();
+
 }
