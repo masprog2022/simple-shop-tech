8cc209bc77bd322601c289288e9f98c66432a3a6
masprog2022 <masprog2022@gmail.com>
2024-08-15 19:10:18 +0100

    feat: create OrderItemDto

--- src/main/java/com/masprogtech/dtos/OrderItemDto.java
+++ src/main/java/com/masprogtech/dtos/OrderItemDto.java
@@ -1,2 +1,13 @@
-package com.masprogtech.dtos;public class OrderItemDto {
+package com.masprogtech.dtos;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class OrderItemDto {
+    private Long productId;
+    private String productName;
+    private int quantity;
+    private BigDecimal price;
 }
