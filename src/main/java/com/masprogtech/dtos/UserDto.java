7e9f858d208c6ff992aeb237fcbe8071fa347cd7
masprog2022 <masprog2022@gmail.com>
2024-08-15 21:42:08 +0100

    feat: create UserDto

--- src/main/java/com/masprogtech/dtos/UserDto.java
+++ src/main/java/com/masprogtech/dtos/UserDto.java
@@ -1,2 +1,15 @@
-package com.masprogtech.dtos;public class UserDto {
+package com.masprogtech.dtos;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class UserDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private List<OrderDto> orders;
+    private CartDto cart;
 }
