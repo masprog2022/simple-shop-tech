dc6485605adaeb028d73301f42ee018efda3b372
masprog2022 <masprog2022@gmail.com>
2024-08-15 12:30:16 +0100

    feat: create IOrderService

--- src/main/java/com/masprogtech/services/order/IOrderService.java
+++ src/main/java/com/masprogtech/services/order/IOrderService.java
@@ -1,2 +1,9 @@
-package com.masprogtech.services.order;public class IOrderService {
+package com.masprogtech.services.order;
+
+
+import com.masprogtech.entities.Order;
+
+public interface IOrderService {
+    Order placeOrder(Long userId);
+    Order getOrder(Long orderId);
 }
